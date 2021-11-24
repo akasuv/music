88b4f689387b5c3997b664200482991d7b11cb99
Wei Su <cs.akasuv@gmail.com>
2021-11-24 22:15:31 +0800

    refactor: only change isPlaying when user clicks

--- src/Player.js
+++ src/Player.js
@@ -7,38 +7,20 @@ import {
   CardMedia,
   IconButton,
   Typography,
-  LinearProgress,
 } from "@mui/material";
-import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import SkipNextIcon from "@mui/icons-material/SkipNext";
-import PauseIcon from "@mui/icons-material/Pause";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlayCircle,
-  faPause,
-  faStepBackward,
-  faStepForward,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPlayCircle, faPause } from "@fortawesome/free-solid-svg-icons";
 import { shuffle } from "lodash";
 import { data } from "./playlist";
 
-const Player = (props) => {
+const playList = shuffle(data);
+const Player = () => {
   const [isPlaying, setIsPlaying] = React.useState(false);
   const [currentIndex, setCurrentIndex] = React.useState(0);
-  const [playList, setPlayList] = React.useState(shuffle(data));
 
   const handlePlay = () => {
     setIsPlaying((prevState) => !prevState);
   };
-
-  // const handlePlayLast = () => {
-  //   if (currentIndex === 0) {
-  //     setCurrentIndex(playList.length - 1);
-  //   } else {
-  //     setCurrentIndex((prevState) => prevState - 1);
-  //   }
-  // };
 
   const handlePlayNext = () => {
     if (currentIndex === playList.length - 1) {
@@ -46,7 +28,6 @@ const Player = (props) => {
     } else {
       setCurrentIndex((prevState) => prevState + 1);
     }
-    setIsPlaying(true);
   };
 
   return (
@@ -83,9 +64,6 @@ const Player = (props) => {
             height: "40px",
           }}
         >
-          {/* <IconButton onClick={handlePlayLast}>
-            <FontAwesomeIcon icon={faStepBackward} />
-          </IconButton> */}
           <IconButton
             onClick={handlePlay}
             sx={{ fontSize: isPlaying ? "24px" : "40px", width: "32px" }}
@@ -96,9 +74,6 @@ const Player = (props) => {
               <FontAwesomeIcon icon={faPlayCircle} />
             )}
           </IconButton>
-          {/* <IconButton onClick={handlePlayNext}>
-            <FontAwesomeIcon icon={faStepForward} />
-          </IconButton> */}
         </Box>
       </Card>
       <Box sx={{ position: "absolute", bottom: "0", zIndex: "-1" }}>
